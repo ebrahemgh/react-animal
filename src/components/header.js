788208cc5c497e25b3a02fd9a4dd27f281285d3b
ebrahemgh <ebrahem.naser98@gmail.com>
2021-05-19 14:19:34 +0300

    finishexd lab01

---
+++ src/components/header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Header extends React.Component {
+    render() {
+        return(
+        <h3> i am the title wallah  </h3>
+        )
+        
+
+    }
+}
+export default Header;
